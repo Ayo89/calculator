802483f9644f5ddf91b3c85f341770554db1c881
Ayo89 <ayose89@gmail.com>
2024-09-09 23:54:36 +0530

    Finish logical functions working version 1.0

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-let value = [];
-let value1 = 0;
-let value2 = 0;
-let operador = "";
+let values = [];
+let tempValues = "";
+let tempResult = 0;
+let result = 0;
+let totalFirstValue = "";
+let totalSecondValue = "";
+let operador = [];
 
+//Add values
 const addValue = (num) => {
-  let number = Number(num);
-  value.push(number);
+  values.push(Number(num));
 };
 
-const totalValue = (arr) => {
-  let result = Number(arr.join(''))
-  return result;
-};
+const tempTotalResult = (event) => {};
 
-const operator = (operator, num1, num2) => {
+//Operate function
+const operator = (operator, totalFirstValue, totalSecondValue) => {
+  console.log(`soy funcion operator ${operator}`);
   const operatos = {
-    "+": (num1, num2) => num1 + num2,
-    "-": (num1, num2) => num1 - num2,
-    "*": (num1, num2) => num1 * num2,
-    "/": (num1, num2) => num1 / num2,
+    "+": (totalFirstValue, totalSecondValue) =>
+      totalFirstValue + totalSecondValue,
+    "-": (totalFirstValue, totalSecondValue) =>
+      totalFirstValue - totalSecondValue,
+    "*": (totalFirstValue, totalSecondValue) =>
+      totalFirstValue * totalSecondValue,
+    "/": (totalFirstValue, totalSecondValue) =>
+      totalFirstValue / totalSecondValue,
   };
 
   if (operatos[operator]) {
-    return operatos[operator](num1, num2);
+    return operatos[operator](totalFirstValue, totalSecondValue);
   } else {
     throw new Error("Operator no support");
   }
@@ -32,6 +38,10 @@ const operator = (operator, num1, num2) => {
 let screenCal = document.createElement("div");
 screenCal.setAttribute("id", "screen");
 
+let textScreen = document.createElement("p");
+screenCal.appendChild(textScreen);
+
+//General container
 let container = document.querySelector("#container");
 
 container.appendChild(screenCal);
@@ -50,12 +60,15 @@ for (let i = 0; i < simbols.length; i++) {
     button.setAttribute("id", "equal");
   }
   button.textContent = simbols[i];
+
+  //Logic buttons
   button.addEventListener("click", (e) => {
     if (
       typeof Number(e.target.textContent) === "number" &&
       !isNaN(e.target.textContent)
     ) {
-      addValue(e.target.textContent);
+      //add value from the user
+      tempValues += e.target.textContent;
     }
     if (
       e.target.textContent === "+" ||
@@ -63,15 +76,61 @@ for (let i = 0; i < simbols.length; i++) {
       e.target.textContent === "-" ||
       e.target.textContent === "*"
     ) {
-      operador = e.target.textContent;
-      value1 = totalValue(value);
-      value = [];
+      console.log(tempResult);
+      //add operador to the stack
+      operador.push(e.target.textContent);
+
+      //check values not is empty and add
+      if (tempValues !== "") {
+        values.push(tempValues);
+        //reset values
+        tempValues = "";
+        console.log(values);
+      }
+      // check have some result in the stack and operate with this
+      if (tempResult > 0) {
+        tempResult = operator(
+          operador[0],
+          Number(tempResult),
+          Number(values[1])
+        );
+        //reset values and save result temp
+        tempValues = "";
+        values = [];
+        values.push(tempResult);
+        operador.shift();
+      }
+      //check if the have 2 value for operate
+      if (values.length === 2) {
+        tempResult = Number(
+          operator(operador[0], Number(values[0]), Number(values[1]))
+        );
+        //reset values and save result temp
+        tempValues = "";
+        values = [];
+        values.push(tempResult);
+        operador.shift();
+      }
     }
+    // take result and save result the next interaction!
     if (e.target.textContent === "=") {
-      value2 = totalValue(value);
-      let result = operator(operador, value1, value2);
-      console.log(result);
+      values.push(tempValues);
+      console.log(`soy values ${values}`);
+      console.log(`soy result y temp result ${result} ${tempResult}`);
+      console.log(values[0], values[1], tempResult);
+      tempResult = operator(
+        operador[0],
+        Number(tempResult > 0 ? tempResult : values[0]),
+        Number(values[1])
+      );
+      //reset values and result save
+      console.log(`result ${result}`);
+      console.log(`tempresult ${tempResult}`);
+      tempValues = "";
+      values = [];
+      values.push(tempResult);
     }
+    //result
   });
   containerButton.appendChild(button);
 }
